f7cd50b00e51ab05243beb1b52c3c466c0fddbe1
Naveen <nveenjain@gmail.com>
2017-01-26 14:51:38 +0530

    Uploading JS file

---
+++ index.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var o = 0;
+app.get('/', function(req, res){
+
+    res.sendfile(__dirname +'/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected' + ++o);
+    socket.on('disconnect', function(){
+        console.log('user disconnected' + --o);
+    });
+});
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+module.exports.o = o;
